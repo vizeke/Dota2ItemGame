b769208655ead61cf0c6b4b1d55c50a396e8ef73
ViZeke <vizeke@gmail.com>
2015-06-01 17:34:20 -0300

    ShoopKeeper Quiz copy + node modules

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+var _cssPattr = "css/*.css";
+var _jsPattr = "controller/*.js";
 
 gulp.task('default', function() {
   // place code for your default task here
+  
 });
+
+gulp.task('css-watch', function(){
+    return gulp.src(_cssPattr).pipe(browserSync.stream());    
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    
+    browserSync.notify('Teste');
+    
+    gulp.watch(_cssPattr, ['css-watch']);
+    gulp.watch(_jsPattr).on("change", browserSync.reload);
+    gulp.watch("*.html").on("change", browserSync.reload);
+});
+
